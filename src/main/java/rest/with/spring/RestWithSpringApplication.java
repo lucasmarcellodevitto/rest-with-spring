c007308ce28d13ae93028285427b08c24a98d176
lucas-luizalabs <lucas.devitto@luizalabs.com>
2018-03-05 10:55:57 -0300

    Adicionando configuração Eureka Clint

--- src/main/java/rest/with/spring/RestWithSpringApplication.java
+++ src/main/java/rest/with/spring/RestWithSpringApplication.java
@@ -2,8 +2,10 @@ package rest.with.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RestWithSpringApplication {
 
 	public static void main(String[] args) {
